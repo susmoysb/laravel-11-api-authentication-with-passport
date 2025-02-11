ee25110b7ab7e4f93fc4a6c59b51f883eb1d7415
susmoysb <susmoysb@gmail.com>
2025-02-12 03:00:25 +0600

    Database configuration done, DatabaseSeeder, UserFactory, User modified and 2 columns added in users table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,9 +25,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => fake()->unique()->userName(),
+            'employee_id' => fake()->unique()->numerify('EMP###'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('12345678'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
